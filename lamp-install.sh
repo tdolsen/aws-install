0c91c6f43559b65eb1bb4b0e53fd11360129af0e
Torkild Dyvik Olsen <torkild@tdolsen.net>
2011-06-18 14:08:12 +0200

    Updated LAMP script with quiet apt installs, and some required sudo's and correct url in comment.

--- lamp-install.sh
+++ lamp-install.sh
@@ -1,19 +1,20 @@
+#!/bin/bash
+
 # Simple install script for setting up a new LAMP stack on a fresh Amazon instance
 #
-# Easiest way to install is probably to put it in public Dropbox and do:
-# curl https://dropbox-link/amazon-lamp-install.sh | sh
-#
+# Easiest way to install is probably running the following line:
+# curl https://github.com/tdolsen/aws-install/raw/master/lamp-install.sh | sh
 
 # Installing the LAMP stack
 sudo apt-get update
-sudo apt-get -y install apache2
-sudo apt-get -y install php5
+sudo apt-get -qqy install apache2
+sudo apt-get -qqy install php5
 
 # Notice that you have to input a root password here!
-sudo apt-get -y install mysql-server
+sudo apt-get -qqy install mysql-server
 
-sudo apt-get -y install imagemagick
-sudo apt-get -y install php5-curl php5-gd php5-imagick php5-json php5-mcrypt php5-memcached php5-mysql php5-suhosin
+sudo apt-get -qqy install imagemagick
+sudo apt-get -qqy install php5-curl php5-gd php5-imagick php5-json php5-mcrypt php5-memcached php5-mysql php5-suhosin
 
 # This part here is to remove the DB2 stuff out of public_html
 mkdir ~/DB2
@@ -24,11 +25,9 @@ sudo touch /var/www/index.html
 # Restart Apache
 sudo apache2ctl restart
 
-# Set up some nice tools...
-apt-get install rcconf
+# Set up some nice tools
+sudo apt-get -qqy install rcconf
 
 # Clean up services
-service db2 stop
-update-rc.d db2 disable
-
-
+sudo service db2 stop
+sudo update-rc.d db2 disable
