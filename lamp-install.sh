a6a16036d57d40043d2b3f853bc1b706eda6a30f
Torkild Dyvik Olsen <torkild@tdolsen.net>
2011-06-21 23:51:27 +0200

    Adjusted script for new AMI, and removed unnecessary code (as well as added mod_rewrite to the default mods-enabled).

--- lamp-install.sh
+++ lamp-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Simple install script for setting up a new LAMP stack on a fresh Amazon instance
-# Tested with AMI ID: ami-f5200a81
+# Tested with AMI ID: ami-4090a634
 #
 # Easiest way to install is probably running the following line:
 # curl https://raw.github.com/tdolsen/aws-install/master/lamp-install.sh | sh
@@ -17,15 +17,8 @@ sudo apt-get -qqy install mysql-server
 sudo apt-get -qqy install imagemagick
 sudo apt-get -qqy install php5-curl php5-gd php5-imagick php5-json php5-mcrypt php5-memcached php5-mysql php5-suhosin
 
-# This part here is to remove the DB2 stuff out of public_html
-mkdir ~/DB2
-sudo mv /var/www/* ~/DB2/
-sudo mv /var/www/.ssh ~/DB2/.ssh
-echo "hello" | sudo tee /var/www/index.html
+# Add mod_rewrite to mods-enabled
+sudo ln -s ../mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
 
 # Restart Apache
 sudo apache2ctl restart
-
-# Clean up services
-sudo service db2 stop
-sudo update-rc.d db2 disable
